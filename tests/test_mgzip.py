4ce8318885a76b52cfd24b3bf7a49d81d83cfa7d
Tim Hughes <thughes@thegoldfish.org>
2021-01-26 23:39:44 +0000

    add github actions matrix for supported python versions

--- tests/test_mgzip.py
+++ tests/test_mgzip.py
@@ -1,5 +1,6 @@
-import pytest
+import sys
 import os
+import pytest
 import mgzip
 import gzip
 
@@ -9,21 +10,22 @@ Simple is better than complex.
 Complex is better than complicated.
 """
 
+
 def test_write_wb(tmpdir):
 
     filename = os.path.join(tmpdir, "test.gz")
-    with mgzip.open(filename, 'wb', compresslevel=6) as f1:
+    with mgzip.open(filename, "wb", compresslevel=6) as f1:
         f1.write(DATA1 * 50)
         # Try flush and fileno.
         f1.flush()
         f1.fileno()
-        if hasattr(os, 'fsync'):
+        if hasattr(os, "fsync"):
             os.fsync(f1.fileno())
         f1.close()
     f1.close()
 
     assert os.path.exists(filename)
-    with gzip.open(filename, 'rb') as f2:
+    with gzip.open(filename, "rb") as f2:
         file_content = f2.read()
     assert file_content == DATA1 * 50
 
@@ -31,23 +33,36 @@ def test_write_wb(tmpdir):
 def test_read_rb(tmpdir):
 
     filename = os.path.join(tmpdir, "test.gz")
-    with gzip.open(filename, 'wb') as f1:
+    with gzip.open(filename, "wb") as f1:
         f1.write(DATA1 * 500)
 
-    with mgzip.open(filename, 'rb') as f2:
+    with mgzip.open(filename, "rb") as f2:
         file_content = f2.read()
     assert file_content == DATA1 * 500
 
 
 def test_pool_close(tmpdir):
-    
-    s = b"1234567890" * 1000
+
     filename = os.path.join(tmpdir, "test.gz")
-    fh = mgzip.open(filename, 'wb', compresslevel=6, thread=4, blocksize=128)
-    fh.write(s)
-    assert repr(fh.pool) == "<multiprocessing.pool.ThreadPool state=RUN pool_size=4>"
+    fh = mgzip.open(filename, "wb", compresslevel=6, thread=4, blocksize=128)
+    fh.write(DATA1 * 500)
+    if sys.version_info >= (3, 8):
+        assert (
+            repr(fh.pool) == "<multiprocessing.pool.ThreadPool state=RUN pool_size=4>"
+        )
     fh.close()
     assert fh.fileobj is None
     assert fh.myfileobj is None
     assert fh.pool_result == []
-    assert repr(fh.pool) == "<multiprocessing.pool.ThreadPool state=CLOSE pool_size=4>"
+    if sys.version_info >= (3, 8):
+        assert (
+            repr(fh.pool) == "<multiprocessing.pool.ThreadPool state=CLOSE pool_size=4>"
+        )
+    if sys.version_info >= (3, 7):
+        with pytest.raises(ValueError) as excinfo:
+            fh.pool.apply(print, ("x",))
+        assert "Pool not running" in str(excinfo.value)
+    else:
+        with pytest.raises(AssertionError) as excinfo:
+            fh.pool.apply(print, ("x",))
+        assert "" == str(excinfo.value)
