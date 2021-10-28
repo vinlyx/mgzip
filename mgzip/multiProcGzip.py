af6778cee16d98a4053706fe345cad46c028e1e2
Craig Milo Rogers <rogers@isi.edu>
2021-10-28 15:25:47 -0700

    Update the version string.

--- mgzip/multiProcGzip.py
+++ mgzip/multiProcGzip.py
@@ -14,7 +14,7 @@ import io
 from gzip import GzipFile, write32u, _GzipReader, _PaddedFile, READ, WRITE, FEXTRA, FNAME, FCOMMENT, FHCRC
 from multiprocessing.dummy import Pool
 
-__version__ = "0.2.1.2"
+__version__ = "0.2.1.3"
 
 SID = b'IG' # Subfield ID of indexed gzip file
 
